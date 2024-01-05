d7945b94a17be6fb3a2742cf5b9c86cda6334fa7
kebinKel <kebinkelv@gmail.com>
2024-01-05 17:47:15 +0100

    commit

--- menu.js
+++ menu.js
@@ -1,14 +1,17 @@
 let menu = document.querySelector('.fa-bars');
 let close = document.querySelector('.close');
+let dim = document.querySelector('.dim');
 let nav = document.querySelector('nav');
 
 menu.addEventListener('click', ()=>{
     nav.style.right = '0';
     menu.style.display = 'none';
+    dim.style.display = 'block';
 })
 
 
 close.addEventListener('click', ()=>{
     nav.style.right = '-100%';
     menu.style.display = 'block';
+    dim.style.display = 'none';
 })
